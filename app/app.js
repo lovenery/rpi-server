f6e276a6f5216e19767fc25b50ac7900c03087e9
Hsu <edittye379@gmail.com>
2017-02-25 06:03:17 +0800

    testing

--- app/app.js
+++ app/app.js
@@ -1,13 +1,20 @@
 // var auth = require('./routes/auth');
 // var example = require('./routes/example');
+const rpio = require('rpio')
 
 module.exports = function (app) {
     app.get('/', function(req, res){
-        res.render('example', { userAgent: req.headers['user-agent'] })
+        res.render('index', { userAgent: req.headers['user-agent'] })
     })
     app.post('/', function (req, res) {
-        res.json(req.body);
-    });
+        res.json(req.body)
+    })
+    app.post('/led', function (req, res) {
+        rpio.init({mapping: 'gpio'});
+        rpio.open(4, rpio.OUTPUT);
+        rpio.write(4, + parseInt(req.body.led, 10));
+        res.json(req.body)
+    })
 
     // app.use('/auth', auth);    
     // app.use('/example', example);
