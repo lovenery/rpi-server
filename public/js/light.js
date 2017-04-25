9023e7114e2beb079fa695d1581e11b8752bb914
Hsu <edittye379@gmail.com>
2017-04-26 05:37:14 +0800

    text

--- public/js/light.js
+++ public/js/light.js
@@ -3,7 +3,8 @@ $('#light').click(function () {
     url: '/light',
     method: 'POST',
     success: function (data) {
-      console.log(data)
+      console.log('light: ' + data)
+      $('#light_text').text(data)
     },
     error: function (data) {
       console.log(data)
